4c85740565f02146db010719e2ac75a7aadfe256
Cristhal Campos <131567720+CristhalCampos@users.noreply.github.com>
2024-06-27 13:58:13 -0400

    calcu mejoras

--- calculadora.js
+++ calculadora.js
@@ -1,41 +1,20 @@
-let numeros = document.querySelectorAll('.numeros');
-let operaciones = document.querySelectorAll('.operaciones');
-let num1 = 0;
-let num2 = 0;
-let oper = ''
-numeros.forEach((numero) => {
-    numero.addEventListener('click', () => {
-        if (document.getElementById('input').value === '0') {
-            document.getElementById('input').value = numero.textContent;
-        } else {
-            document.getElementById('input').value += numero.textContent;
-        }
-    })
-})
-operaciones.forEach((operacion) => {
-    operacion.addEventListener('click', () => {
-        if (operacion === 'AC') {
-            document.getElementById('input').value = '0';
-        } else if (operacion === 'C') {
-            document.getElementById('input').value = document.getElementById('input').value.slice(0, -1);
-        } else {
-            num1 = parseFloat(document.getElementById('input').value);
-            oper = operacion.textContent;
-            document.getElementById('input').value = '';
-        }
-    }) 
-})
-document.getElementById('igual').addEventListener('click', () => {
-    num2 = parseFloat(document.getElementById('input').value);
-    if (oper === '+') {
-        document.getElementById('input').value = num1 + num2;
-    } else if (oper === '-') {
-        document.getElementById('input').value = num1 - num2;
-    } else if (oper === '*') {
-        document.getElementById('input').value = num1 * num2;
-    } else if (oper === '/') {
-        document.getElementById('input').value = (num1 / num2).toFixed(8);
-    } else if (oper === '^') {
-        document.getElementById('input').value = num1 ** num2;
+let oper = '';
+function calcular(c) {
+    if (c == 'AC') {
+        oper = '';
+    } else if (c == 'C') {
+        oper = oper.slice(0, oper.length - 1);
+    } else if (c == '=') {
+        oper = String(eval(oper));
+    } else {
+        oper += c;
     }
-})
+    document.querySelector('#input').value = oper;
+}
+function modo(){
+    document.querySelector("body").classList.toggle("bg-zinc-700");
+    document.querySelector("#botones").classList.toggle("bg-blue-950");
+    document.querySelector("#modo").classList.toggle("bg-blue-950");
+    document.querySelector("#calculadora").classList.toggle("bg-black");
+    document.getElementById('modo').textContent = document.getElementById('modo').textContent == '☀' ? '🌘' : '☀';
+  }
